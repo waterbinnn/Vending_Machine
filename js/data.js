ff83d775c65177211cf8b02dbb11022bd8312d72
waterbinnn <ewaterbin@naver.com>
2022-09-08 14:10:41 +0900

    Fix: json 파일 content 수정

--- js/data.js
+++ js/data.js
@@ -1,34 +1,8 @@
-const data = [
-  {
-    flavor: "original",
-    stock: 4,
-    price: 1000,
-  },
-  {
-    flavor: "violet",
-    stock: 4,
-    price: 1000,
-  },
-  {
-    flavor: "yellow",
-    stock: 5,
-    price: 1000,
-  },
-  {
-    flavor: "cool",
-    stock: 3,
-    price: 1000,
-  },
-  {
-    flavor: "green",
-    stockr: 5,
-    price: 1000,
-  },
-  {
-    flavor: "orange",
-    stock: 4,
-    price: 1000,
-  },
+export default [
+    { id: 0, name: 'Original_Cola', stock: 4, price: 1000, buy: 0, obtain: 0 },
+    { id: 1, name: 'Violet_Cola', stock: 4, price: 1000, buy: 0, obtain: 0 },
+    { id: 2, name: 'Yellow_Cola', stock: 5, price: 1000, buy: 0, obtain: 0 },
+    { id: 3, name: 'Cool_Cola', stock: 3, price: 1000, buy: 0, obtain: 0 },
+    { id: 4, name: 'Green_Cola', stockr: 5, price: 1000, buy: 0, obtain: 0 },
+    { id: 5, name: 'Orange_Cola', stock: 4, price: 1000, buy: 0, obtain: 0 },
 ];
-
-export default data;
